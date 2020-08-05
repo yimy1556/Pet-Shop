af32c7bfabf13898741f2a686a766afc3f602cf1
Yimy LLanos <llanosyimy@gmail.com>
2020-08-05 19:19:47 -0300

    Disenio preliminar terminado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
+const url = 'https://apipetshop.herokuapp.com/api/articulos'
+const productsMed = 'Medicamento'
+const ProductsJuguetes = 'Juguete'
+
 var app = new Vue({
     el: '#app',
     data: {
-        numberOfPets:0
+        pagina:'',
+        filtroStock:'',
+        filterPrecio:'',
+        listaDeMedicamento:[],
+        listaDeJuguetes:[],
+        show:false
     },
     computed: {
+        articulosFinales(){
+            if(this.filterPrecio == this.filtroStock) return this[`listaDe${this.pagina}s`]
+            let articulos = (this.filtroStock == '')? this[`listaDe${this.pagina}s`].filter(art => art.stock <= 5):this[`listaDe${this.pagina}s`]    
+            (this.filterPrecio != '') && articulos.sort((a,b) => a.precio - b.precio)
+            return (filterPrecio == 'ME')? articulos.reverse():articulos
+        },
+        descri(){
+            return (!this.show)? 'Mostrar Descripcion': 'Ocultar Descripcion'
+        }
     }
 })
+
+const loadProduc = (products) => app[`listaDe${app.pagina}s`] = products.filter( product => product.tipo == app.pagina)
+
+$(document).ready(
+    $.ajax({
+        url: url,
+        success: function (articulos){
+            app.pagina = (document.querySelector('#medicamentos'))? productsMed:ProductsJuguetes
+            loadProduc(articulos.response)
+            console.log(articulos.response)
+        }
+    })
+)
